eb1c523f06761432f1a79b354ef205d2fae8b1a3
Ravinder <ravinder729090@gmail.com>
2025-06-06 20:40:53 +0530

    fix: simplify CORS configuration in app.js; remove unused whitelist and options for cleaner setup

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,19 +4,19 @@ import cors from 'cors';
 
 const app = express();
 
-const whitelist = ['http://localhost:5173', 'https://task-manager-three-swart.vercel.app' || process.env.CORS_ORIGIN]
-const corsOptions = {
-    origin: function (origin, callback) {
-        if (whitelist.indexOf(origin) !== -1 || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    credentials: true
-}
+// const whitelist = ['http://localhost:5173', 'https://task-manager-three-swart.vercel.app' || process.env.CORS_ORIGIN]
+// const corsOptions = {
+//     origin: function (origin, callback) {
+//         if (whitelist.indexOf(origin) !== -1 || !origin) {
+//             callback(null, true);
+//         } else {
+//             callback(new Error('Not allowed by CORS'));
+//         }
+//     },
+//     credentials: true
+// }
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
