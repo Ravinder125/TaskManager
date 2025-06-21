dfd786fcd2ab55fc29fad742dc626d4888b9f839
Ravinder <ravinder729090@gmail.com>
2025-06-21 15:25:12 +0530

    fix: update logout component to use Loading component and remove unnecessary withCredentials option

--- frontend/src/pages/Auth/Logout.jsx
+++ frontend/src/pages/Auth/Logout.jsx
@@ -2,16 +2,14 @@ import React, { useEffect } from 'react'
 import axiosInstance from '../../utils/axiosInstance'
 import { API_PATHS } from '../../utils/apiPaths'
 import { useNavigate } from 'react-router-dom'
+import Loading from '../../components/index'
 
 const Logout = () => {
     const navigate = useNavigate();
     useEffect(() => {
         const handleLogout = async () => {
             try {
-                await axiosInstance.get(
-                    API_PATHS.AUTH.LOGOUT, { withCredentials: true }
-                );
-
+                await axiosInstance.get(API_PATHS.AUTH.LOGOUT);
                 navigate('/login')
             } catch (error) {
                 console.error('Error logout the user:', error)
@@ -20,7 +18,7 @@ const Logout = () => {
         handleLogout();
     }, [])
     return (
-        <div>Loading</div>
+        <Loading />
     )
 }
 
