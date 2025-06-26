7184878a73c2c2d52a10a14c3710d016918e6e5f
Ravinder <ravinder729090@gmail.com>
2025-06-26 18:08:35 +0530

    fix: update Start component layout for improved responsiveness and alignment

--- frontend/src/pages/Start.jsx
+++ frontend/src/pages/Start.jsx
@@ -4,7 +4,7 @@ import hero from '../assets/hero.webp'
 const Start = () => {
     return (
         <div >
-            <header className='start-container m-10 flex justify-between align-center px-2'>
+            <header className='start-container flex flex-col sm:flex-row justify-between items-center px-2'>
                 <div>
                     <h3 className='text-2xl font-semibold '>Task Manager</h3>
                 </div>
@@ -19,7 +19,7 @@ const Start = () => {
             </header>
             <main>
                 <div className='start-container grid sm:grid-cols-2 grid-cols-1 items-center justify-center p-6'>
-                    <div className='order-2 sm:order-1'>
+                    <div className='order-2 mb-3 mt-3 sm:mt-0 sm:mb-0 sm:order-1'>
                         <h1 className='text-6xl font-bold text-primary'>TASK</h1>
                         <h2 className='text-3xl font-semibold'>MANAGER</h2>
                         <p className='mt-3 mb-6 text-xs text-slate-700'>Take control of your productivity with our powerful task manager. Whether you're managing personal goals or leading a team, our intuitive platform helps you create, assign, and track tasks effortlessly—all in one place. Prioritize what matters, stay focused, and achieve more every day</p>
@@ -34,7 +34,7 @@ const Start = () => {
                         />
                     </div>
                 </div>
-                
+
             </main>
         </div>
     )
