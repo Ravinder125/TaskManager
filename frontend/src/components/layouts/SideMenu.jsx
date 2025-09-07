b4e1386848971674672aa5dec0fd902e31812c7e
Ravinder <ravinder729090@gmail.com>
2025-09-07 15:23:55 +0530

    side bar dark functionality finish

--- frontend/src/components/layouts/SideMenu.jsx
+++ frontend/src/components/layouts/SideMenu.jsx
@@ -62,7 +62,7 @@ const SideMenu = ({ activeMenu }) => {
                             key={`menu_${idx}`}
                             className={`w-full  flex items-center gap-4 text-[15px] 
                     ${activeMenu == item.label
-                                    ? "text-primary bg-linear-to-r from-blue-100/40 to-blue-400/50 border-r-3 dark:text-[var(--dark-primary)]"
+                                    ? "text-primary bg-linear-to-r from-blue-100/50 dark:from-blue-400/40  dark:to-blue-800/50 to-blue-100/50 border-r-3 dark:text-[var(--dark-primary)]"
                                     : ''} py-3 px-6 mb-3 cursor-pointer`}
                             onClick={() => handleClick(item.path)}
                         >
