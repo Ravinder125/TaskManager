359c890e80f386f6e78600174dc3f5b1ad2ee08d
Ravinder <ravinder729090@gmail.com>
2025-09-10 12:05:19 +0530

    fix layout bugs

--- frontend/src/components/layouts/DashboardLayout.jsx
+++ frontend/src/components/layouts/DashboardLayout.jsx
@@ -11,16 +11,16 @@ const DashboardLayout = ({ children, activeMenu }) => {
             <Navbar activeMenu={activeMenu} />
 
             {user && (
-                <div className='w-screen grid grid-cols-1 lg:grid-cols-12 h-full overflow-hidden'>
+                <div className='w-screen grid grid-cols-1 xl:grid-cols-12 lg:grid-cols-16 px-3 h-full overflow-hidden'>
                     {/* SideMenu: hidden on small screens, visible on lg+ */}
-                    <div className='hidden lg:block w-full h-full rounded-2xl overflow-hidden lg:col-span-3 xl:col-span-3 2xl:col-span-2 m'>
-                        <div className='mx-4 mt-5'>
+                    <div className='hidden lg:block w-full h-full rounded-2xl overflow-hidden lg:col-span-4 xl:col-span-2'>
+                        <div className='w-full mt-5'>
                             <SideMenu activeMenu={activeMenu} />
                         </div>
                     </div>
 
                     {/* Main content: full width on small screens, reduced on lg+ */}
-                    <main className='w-full col-span-1 lg:col-span-9 xl:col-span-9 2xl:col-span-10 mx-2 sm:mx-4 lg:mx-5 h-[calc(100vh-61px)] overflow-auto'>
+                    <main className='shrink-0 lg:px-2 w-full col-span-1 lg:col-span-12 xl:col-span-10 sm:mx-0 h-[calc(100vh-61px)] overflow-auto'>
                         {children}
                     </main>
                 </div>
